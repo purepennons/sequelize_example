26eb147288d136ab29a320d481795833b248ca6a
Chiahao Lin <y80811@gmail.com>
2015-11-12 22:06:46 +0800

    add create user api

--- model/User.js
+++ model/User.js
@@ -4,12 +4,26 @@ module.exports = function(sequelize, DataTypes) {
 
   // 每個 model 可以看成一個 table
   var User = sequelize.define("User", {
-    username: DataTypes.STRING(255),
+    user_id: {
+      type: DataTypes.INTEGER(11),
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING(255),
+      unique: true
+    },
     password: DataTypes.STRING(255),
     first_name: DataTypes.STRING(255),
     last_name: DataTypes.STRING(255),
     Gender: DataTypes.ENUM('', 'Male', 'Female'),
-    joinDate: DataTypes.DATE
+    joinDate: DataTypes.DATE,
+  }, {
+    // 拿掉 Sequelize 預設的 CreatedAt 與 UpdatedAt 欄位
+    timestamps: false,
+    // 避免 Sequelize 自動把 table 名稱 + s
+    freezeTableName: true,
+    // 自定義 table name
+    tableName: 'member'
   });
 
   return User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,56 @@
 var express = require('express');
+var db      = require('../model/db');
 var router = express.Router();
 
-/* GET users listing. */
+// 讀出全部 Users 資料
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db.User.findAll()
+  .then(function(users) {
+    // console.log(users[0].dataValues);
+    res.json({
+      status: 'success',
+      description: 'query all users',
+      data: users
+    });
+  })
+  .catch(function(err) {
+    res.json({
+      status: 'error',
+      description: 'something wrong when querying all users.',
+      data: []
+    });
+  });
+});
+
+// 新增 User
+router.post('/create', function(req, res, next) {
+  var user = {
+    user_id: req.body.user_id,
+    username: req.body.username,
+    password: req.body.password,
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    Gender: req.body.Gender,
+    joinDate: new Date()
+  };
+
+  // insert a user
+  db.User.create(user)
+  .then( () => { // es6 語法，等價於 function，偷懶可以這樣寫
+    console.log('create a user');
+    res.json({
+      status: 'success',
+      description: 'create a user',
+      data: []
+    });
+  })
+  .catch( err => {
+    res.json({
+      status: 'error',
+      description: 'something wrong when inserting a user.',
+      data: []
+    });
+  });
 });
 
 module.exports = router;
